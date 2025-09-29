19b4bea237afed233c1d8b2ae5bfbc33c9789985
cvcvka5 <cvcvka5@gmail.com>
2025-09-29 18:32:50 +0200

    categorized and reshaped tests

--- tests/test_httpcrawler.py
+++ tests/test_httpcrawler.py
@@ -1,41 +1,96 @@
 import pytest
 import requests
-from crawl2schema.crawler.http import SyncHTTPCrawler
+from crawl2schema.crawler.http import SyncHTTPCrawler, CrawlerSchema
 
-base_url = "https://web-scraping.dev/products"
-pagination_url = "https://web-scraping.dev/products?page={page}"
-headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36"}
+def as_new_section(func):
+    def inner():
+        print(f"\n\n---\t{func.__name__}\t---\n")
+        func()
+        print(f"\n---\tEND\t---")
+    return inner
 
+BASE_URL = "https://web-scraping.dev/products"
+HEADERS = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36"
+}
+
+@as_new_section
 def test_webpage_live():
-    assert requests.get(base_url).status_code == 200
-    
+    """Ensure the webpage is up."""
+    assert requests.get(BASE_URL, headers=HEADERS).status_code == 200
+    print("Webpage is LIVE.")
 
-shallow_crawler_schema = {
-    "baseSelector": "div.products",
-    "targetElements": [
-        {"name": "name", "type": str, "selector": "div.description > h3 > a", "formatter": lambda name: name.strip().lower()},                           
-        {"name": "href", "type": str, "selector": "div.description > h3 > a", "attribute": "href", "formatter": lambda url: url.replace("https://web-scraping.dev", "")},                           
-        {"name": "short-description", "type": str, "selector": "div.short-description", "formatter": lambda desc: desc.strip().upper()[:30].strip()},
-        {"name": "price", "type": float, "selector": "div.price"}                          
+shallow_crawler_schema: CrawlerSchema = {
+    "base_selector": "div.product",
+    "fields": [
+        {"name": "name", "type": "text", "selector": "div.description > h3 > a", "formatter": lambda name: name.strip().lower()},
+        {"name": "href", "type": "text", "selector": "div.description > h3 > a", "attribute": "href", "formatter": lambda url: url.replace("https://web-scraping.dev", "")},
+        {"name": "short_description", "type": "text", "selector": "div.short-description", "formatter": lambda desc: desc.strip().upper()[:30].strip()},
+        {"name": "price", "type": "number", "selector": "div.price"},
     ]
 }
+
+@as_new_section
 def test_sync_shallow_httpcrawler():
     sync_crawler = SyncHTTPCrawler()
+    products = sync_crawler.fetch(BASE_URL, schema=shallow_crawler_schema, headers=HEADERS)
     
-    products_data_1 = sync_crawler.fetch(base_url, crawler_schema=shallow_crawler_schema, headers=headers)
     
-    assert len(products_data_1) == 5
-    assert list(products_data_1[0].keys()) == [elDict["name"] for elDict in shallow_crawler_schema.get("targetElements", [])]
-    assert None not in products_data_1[0].values()
-    assert len(products_data_1[0]["short-description"]) <= 30
+    print(len(products), products)
     
-def test_sync_shallow_paginated_httpcrawler():
+    assert len(products) == 5
+    for product in products:
+        assert all(field["name"] in product for field in shallow_crawler_schema["fields"])
+        assert None not in product.values()
+        assert len(product["short_description"]) <= 30
+
+product_schema: CrawlerSchema = {
+    "base_selector": "script#reviews-data",
+    "fields": [
+        {"name": "reviews", "type": "json", "selector": "*"}
+    ]
+}
+
+@as_new_section
+def test_sync_product_reviews():
     sync_crawler = SyncHTTPCrawler()
+    product_url = f"{BASE_URL[:-1]}/5"
+    data = sync_crawler.fetch(product_url, schema=product_schema, headers=HEADERS)
     
-    paginated_products = sync_crawler.paginated_fetch(base_url=pagination_url, start_page=1, end_page=5, crawler_schema=shallow_crawler_schema, headers=headers)
+    print(data)
     
-    assert len(paginated_products) == 25
-    assert list(paginated_products[10].keys()) == [elDict["name"] for elDict in shallow_crawler_schema.get("targetElements", [])]
-    assert None not in paginated_products[10].values()
-    assert len(paginated_products[10]["short-description"]) <= 30
+    assert len(data) > 0
+    for record in data:
+        assert "reviews" in record
+        assert isinstance(record["reviews"], list)
+
+deep_crawler_schema: CrawlerSchema = {
+    "base_selector": "div.product",
+    "fields": [
+        {"name": "name", "type": "text", "selector": "div.description > h3 > a", "formatter": lambda name: name.strip().lower()},
+        {"name": "href", "type": "text", "selector": "div.description > h3 > a", "attribute": "href", "formatter": lambda url: url.replace("https://web-scraping.dev", "")},
+        {"name": "short_description", "type": "text", "selector": "div.short-description", "formatter": lambda desc: desc.strip().upper()[:30].strip()},
+        {"name": "price", "type": "number", "selector": "div.price"},
+        {"name": "details", "type": "text", "selector": "div.description > h3 > a", "attribute": "href", "follow_schema": product_schema}
+    ]
+}
+
+@as_new_section
+def test_sync_url_follow_httpcrawler():
+    sync_crawler = SyncHTTPCrawler()
+    data = sync_crawler.fetch(BASE_URL, schema=deep_crawler_schema, headers=HEADERS)
     
+    print(data)
+    
+    assert len(data) == 5
+    for product in data:
+        assert "reviews" in product
+        assert isinstance(product["reviews"], list)
+
+# Run all tests when executing directly
+if __name__ == "__main__":
+
+    test_webpage_live()
+    test_sync_shallow_httpcrawler()
+    test_sync_product_reviews()
+    test_sync_url_follow_httpcrawler()
